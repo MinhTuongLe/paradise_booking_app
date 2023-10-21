34c237c67ddbb9a96c8a656500d1ed6cedabdc69
LeMinhTuong <leminhtuong09122002@gmail.com>
2023-10-22 00:03:36 +0700

    update

--- src/pages/PlaceDetail/index.jsx
+++ src/pages/PlaceDetail/index.jsx
@@ -6,17 +6,18 @@ import { useEffect, useState } from "react";
 
 const PlaceDetailPage = () => {
   const params = useParams();
-  console.log(params.id);
+
   const { data } = useGetPlaceByIdQuery({
     id: params.id,
   });
+
   const [placeDetail, setPlaceDetail] = useState();
   useEffect(() => {
     if (data) {
       setPlaceDetail(data.data);
     }
   }, [data]);
-  console.log(placeDetail);
+
   return (
     <>
       {placeDetail && (
@@ -24,44 +25,6 @@ const PlaceDetailPage = () => {
           <Typography.Title level={3} style={{ textAlign: "start" }}>
             {placeDetail.name}
           </Typography.Title>
-          {/* <div>
-            <Space>
-              <Carousel
-                autoplay
-                autoplaySpeed={5000}
-                style={{ width: "300px", height: "300px" }}
-              >
-                <div>
-                  <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    style={{ width: "100%", height: "100%" }}
-                  />
-                </div>
-                <div>
-                  <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    style={{ width: "100%", height: "100%" }}
-                  />
-                </div>
-                <div>
-                  <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    style={{ width: "100%", height: "100%" }}
-                  />
-                </div>
-                <div>
-                  <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    style={{ width: "100%", height: "100%" }}
-                  />
-                </div>
-              </Carousel>
-            </Space>
-          </div> */}
           <Space>
             <Carousel
               autoplay
@@ -98,7 +61,10 @@ const PlaceDetailPage = () => {
               </div>
             </Carousel>
           </Space>
-          <Space direction="vertical" style={{ textAlign: "start" }}>
+          <Space
+            direction="vertical"
+            style={{ textAlign: "start", width: "100%" }}
+          >
             <Typography.Text level={3} style={{ textAlign: "start" }}>
               {placeDetail.description}
             </Typography.Text>
